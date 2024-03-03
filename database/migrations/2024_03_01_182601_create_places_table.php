ac409a1f768ea2efb63fe43a1b07c012b6b4e93e
Gha-zy001 <axxgha.zy01@gmail.com>
2024-03-03 15:19:46 +0200

    ll

--- database/migrations/2024_03_01_182601_create_places_table.php
+++ database/migrations/2024_03_01_182601_create_places_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
       $table->string('name');
       $table->longText('description');
       $table->string('address');
-      $table->tinyInteger('rating')->nullable();
       $table->text('comment')->nullable();
       $table->timestamps();
     });
